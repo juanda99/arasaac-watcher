2360465399dc2bb59c9f55487fe69d138a2f7675
juanda <juandacorreo@gmail.com>
2017-11-08 00:02:52 +0100

    all code refactored. Need to define getZipFile function

--- index.js
+++ index.js
@@ -30,16 +30,16 @@ watcher
   // .on('addDir', path => log(`Directory ${path} has been added`))
   .on('add', (file) => {
     log(`ADDED FILE: ${file}`)
-    addTask(file, INCLUDE)
+    addFile(file, INCLUDE)
   })
   .on('change', (file) => {
     log(`CHANGED FILE: ${file}`)
-    addTask(file, EXCLUDE) // we remove it and then we add it
-    addTask(file, INCLUDE)
+    addFile(file, EXCLUDE) // we remove it and then we add it
+    addFile(file, INCLUDE)
   })
   .on('unlink', (path) => {
     log(`REMOVED FILE: ${path}`)
-    addTask(file, EXCLUDE)
+    addFile(file, EXCLUDE)
   })
   .on('error', error => log(`WATCHER ERROR: ${error}`))
   .on('ready', () => {
@@ -62,7 +62,7 @@ const initMaterial = (materialId) => {
 }
 
 
-const includeFiles = (file, operation) =>{
+const addFile = (file, operation) =>{
   let materialId = path.dirname(file).split(path.sep)[0]
   // if not initialized, we do it before pushing data
   if (!materialFiles[materialId]) initMaterial(materialId)
@@ -98,8 +98,8 @@ var operateDebounced = _.wrap(
 )
 
 sync = (material, languages) => {
-  // depending in where the file is added we will generate images or zip files:
-  material.excludeScreenshots.map((file)=> {
+  // remove files from screenshosts_300
+  material.excludeScreenshots.map((file) => {
     file.replace('screenshots', `_${process.env.RESOLUTION}`)
     // delete the file from screenshots)
     fs.remove(path.resolve(materials, file), err => {
@@ -107,8 +107,99 @@ sync = (material, languages) => {
       else console.log(`REMOVE FILE: ${file}`)
     })
   })
+  // add screenshots to screenshots_300
+  material.includeScreenshots.map((file) => resizeImage (file, material.materialId, process.env.RESOLUTION))
+
+  // make a fileList per language
+  let fileList = {}
+  material.include.forEach( (item)=>
+    item.languages.forEach( (language)=> {
+      if (!fileList[language]) fileList[language]={include: [], exclude: []}
+      fileList[language].include.push(item.file)
+    })
+  )
+  material.exclude.forEach( (item)=>
+    item.languages.forEach( (language)=> {
+      if (!fileList[language]) fileList[language]={include: [], exclude: []}
+      fileList[language].exclude.push(item.file)
+    })
+  )
+
+  // zip files 
+  zipFiles (fileList, material.idMaterial)
+
+  const zipFiles = async (fileList, materialId) => {
+    // get all the ZipFiles per language
+    try {
+      let zipFiles = await getZipFiles(materialId)
+      for (const [language, files] of Object.entries(fileList)) {
+        // zip file for key language
+        let languageZipfiles = getZipFile(language, files)
+        switch (languageZipFiles.length) {
+          case 0:
+            // new zip file
+            // as zip is not yet created, we don't take into account exclude files though they shouldn't exist!
+            let newZip = path.resolve(materials, materialId, `index-${language}-${uuidv4()}.zip`)
+            let memZip = new AdmZip()
+            files.include.map((file) => memZip.addLocalFile(file))
+            memZip.writeZip(newZip)
+            break;
+          case 1:
+            // we make a new zip file with this value
+            let oldZip = path.resolve(materials, materialId, languageZipfiles[0])
+            let memZip = new AdmZip(oldZip)
+            files.exclude.map((file) => memZip.deleteFile(file))
+            files.include.map((file) => memZip.addLocalFile(file))
+            memZip.writeZip;
+            await fs.move(oldZip, newZip)
+            break;
+          default:
+            throw new Error(`Found more than one zip file for language ${language} in materials ${materialId} folder`);
+        }
+      }
+      //should be just one file!
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const addFilesToZip = async (materialId, language) => {
+    try {
+      let files = await getZipFiles(materialId, language)
+      let newZip = path.resolve(materials, materialId, `index-${language}-${uuidv4()}.zip`)
+      switch (files.length) {
+        case 0:
+          // new zip file
+          let memZip = new AdmZip()
+          filesToZip = await getFilesToZip (materialId, language)
+          filesToZip.map((file) => memZip.addLocalFile(file))
+          memZip.writeZip(newZip)
+          break;
+        case 1:
+          // we make a new zip file with this value
+          let oldZip = path.resolve(materials, materialId, files[0])
+          let zip = new AdmZip(oldZip)
+          filesToZip = await getFilesToZip(materialId, language)
+          let zipEntries = zip.getEntries();
+          filesToZip.map((file) => zipEntries.includes(file) || memZip.addLocalFile(file))
+          zip.writeZip;
+          await fs.move(oldZip, newZip)
+          break;
+        default:
+          throw new Error(`Found more than one zip file for language ${language} in materials ${materialId} folder`);
+      }
+      //should be just one file!
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
 
   material.exclude.map((file)=>
+    
+
+
     let relativeDir = path.dirname(file)
     let dir = `${materials}/${relativeDir}`
     let dirName = dir.split(path.sep).pop()
@@ -167,6 +258,9 @@ const resizeImages = (file, materialId, size) => {
   }
 }
 
+
+
+/*
 const addFilesToZip = async (materialId, language) => {
   try {
     let files = await getZipFiles(materialId, language)
@@ -198,6 +292,8 @@ const addFilesToZip = async (materialId, language) => {
   }
 }
 
+*/
+
 const getZipFiles = async (materialId, language) => {
   try {
     let files = await fs.readdir(path.resolve(materials, materialId))
@@ -208,6 +304,7 @@ const getZipFiles = async (materialId, language) => {
   }
 }
 
+/*
 const getFilesToZip = async (materialId, language) => {
   try {
     let generalFiles = await fs.readdir(path.resolve(materials, materialId))
@@ -219,3 +316,4 @@ const getFilesToZip = async (materialId, language) => {
     console.log(error)
   }
 }
+*/
